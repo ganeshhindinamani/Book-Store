978c5a484c1842e5a647c02c308067bb32cc3d37
Ganesh Hindinamani <ganhindinamani@gmail.com>
2022-01-02 01:24:57 +0530

    env updated

--- src/lib/commerce.js
+++ src/lib/commerce.js
@@ -1,6 +1,11 @@
 import Commerce from "@chec/commerce.js";
 
 export const commerce = new Commerce(
-  process.env.REACT_APP_CHEC_PUBLIC_KEY,
+  "pk_25882d9f32ec33825f3609fba3d05465b531130bbb58b",
   true
 );
+
+// export const commerce = new Commerce(
+//   process.env.REACT_APP_CHEC_PUBLIC_KEY,
+//   true
+// );
